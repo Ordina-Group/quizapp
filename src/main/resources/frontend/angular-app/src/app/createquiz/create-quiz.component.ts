22ca9c8239cae8de730b71275217310d85a38823
Eefje Post <eefje.post@ordina.nl>
2020-07-31 09:54:06 +0200

    onclick functie aangepast zodat deze de vraagt logt in de console

--- src/main/resources/frontend/angular-app/src/app/createquiz/create-quiz.component.ts
+++ src/main/resources/frontend/angular-app/src/app/createquiz/create-quiz.component.ts
@@ -3,10 +3,11 @@ import {AbstractControl, FormBuilder, FormGroup} from "@angular/forms";
 import {Quiz} from "../model/quiz";
 import {Question} from "../model/question";
 import {FormArray} from '@angular/forms';
-import {AnswerIsCorrect} from "../model/answerIsCorrect";
+
 import {AnswerOption} from "../model/answerOption";
 import { QuizService } from '../services/quiz.service';
-import {checkBudgets} from "@angular-devkit/build-angular/src/angular-cli-files/utilities/bundle-calculator";
+import {filter} from "rxjs/operators";
+
 
 
 @Component({
@@ -19,18 +20,24 @@ export class CreateQuizComponent implements OnInit {
   quiz: Quiz;
   quizForm: FormGroup;
   count: number;
-
   newQuiz: Quiz;
   currentQuestion: number;
   lockQuizName: boolean;
+  show: boolean;
   currentQuestionsArray: Array<number>;
+  stored: Array<any>;
+  filteredItem: Question;
+
 
   constructor(private formBuilder: FormBuilder, private quizService: QuizService) {
     this.count = 0;
-    this.currentQuestion = 0;
     this.currentQuestionsArray = [];
     this.lockQuizName = false;
+    this.show = true;
     this.newQuiz = {questions: []} as Quiz;
+    this.stored = [];
+
+
 
     this.quizForm = this.formBuilder.group({
       quizName: [''],
@@ -77,8 +84,8 @@ export class CreateQuizComponent implements OnInit {
 
   saveQuestion() {
     let quizname = this.quizName;
-    let questionDescription = this.quizForm.get('question').value;
 
+    let questionDescription = this.quizForm.get('question').value;
     let question = {answerOptions: []} as Question;
     this.answerOptions.controls.forEach(control => {
       let answerOption = this.toAnswerOption(control)
@@ -89,40 +96,43 @@ export class CreateQuizComponent implements OnInit {
     this.newQuiz.quizDescription = quizname;
     this.newQuiz.questions.push(question);
 
-    let newQuizQuestion = questionDescription;
-    localStorage.setItem(newQuizQuestion, questionDescription);
+    this.count++;
+    console.log('nu is count' + this.count);
+    question.id = this.count;
 
-    let myItem = localStorage.getItem(newQuizQuestion);
+    let q ={id: this.count, qdescription: questionDescription}
+
+
+    this.stored.push(q);
+
+    //this.stored.push(this.count, questionDescription);
+    localStorage.setItem('questions', JSON.stringify(this.stored));
+    let myItem = JSON.parse(localStorage.getItem('questions'));
+
     console.log(myItem);
 
-   // let answerObj ={answerOptions: "answerOption"};
-    //localStorage.setItem('answerObj', JSON.stringify(answerObj));
 this.checkQuestion();
-
   }
 
 
+  //deze functie zorgt ervoor dat de button met vraagnummer boven de vraag verschijnt
   checkQuestion(){
-    this.currentQuestion++;
-    this.count = this.currentQuestion;
-    this.currentQuestionsArray.push(this.count);
+    this.currentQuestionsArray.push(this.count)
   }
 
-  onClick(){
-    const number = this.count;
-    console.log(this.count)
-    console.log(this.quiz.id)
-    this.quizService.getInitQuiz(this.quiz.id)
 
-    // this.quizService.quizSubject.subscribe(oldquiz => { this.quiz = oldquiz
-    //console.log("quiz is")
-    //console.log(oldquiz)})
-    // this.quiz.questions= this.oldQuestionArray;
-    //   console.log(this.oldQuestionArray)
+  //Onclick functie logt dus de juiste vraagomschrijving in de F12 console als je op de button klikt boven die boven de vraag verschijnt.
+  // Volgende stap is om dit nu te laten zien in het inputfield zodat je dit aan kan passen.
+  onClick(questionNumber){
+    console.log(questionNumber);
+   let myFilter = JSON.parse(localStorage.getItem('questions')).filter(questions =>questions.id === questionNumber);
+   console.log(myFilter);
 
   }
 
-  private toAnswerOption(ac: AbstractControl): AnswerOption {
+
+
+  toAnswerOption(ac: AbstractControl): AnswerOption {
     let answerOption = {} as AnswerOption;
     answerOption.value = ac.get('answer').value;
     answerOption.correctAnswer = ac.get('iscorrect').value;
